82a1e7387228d232738726c1dc5f6e48e2b11ecb
guodp <guodp9u0@gmail.com>
2019-03-19 17:52:15 +0800

    update

---
+++ src/graph/breadthFirstSearch.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-unused-vars */
+const {
+  Graph,
+  GraphVertex,
+  GraphEdge
+} = require('./Graph');
+
+const DefaultCallbacks = {
+
+}
+
+/**
+ * bfs graph
+ * @param {Graph} graph graph to search
+ * @param {Object} callbacks callbacks when traverse
+ */
+function bfs(graph, callbacks=DefaultCallbacks) {
+
+}
+
+module.exports = bfs;
---
+++ src/graph/depthFirstSearch.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+const {
+  Graph
+} = require('./Graph');
+
+const DefaultCallbacks = {
+
+}
+
+/**
+ * dfs graph
+ * @param {Graph} graph graph to search
+ * @param {Object} callbacks callbacks when traverse
+ */
+function dfs(graph, callbacks = DefaultCallbacks) {
+
+}
+
+module.exports = dfs;
