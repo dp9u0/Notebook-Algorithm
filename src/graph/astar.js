b2ca44766abf7e64e35210c25cfc9795218f85d6
guodp <guodp9u0@gmail.com>
2019-03-26 15:45:18 +0800

    graph : add astar

--- src/graph/astar.js
+++ src/graph/astar.js
@@ -1,20 +1,63 @@
+const {
+  Graph,
+  GraphVertex,
+  GraphEdge
+} = require('./Graph');
+
+const PriorityQueue = require('../heap/PriorityQueue');
+
 /**
  * ShortestPath
- * @typedef  {Object} Point point index 
- * @property {number} x point
- * @property {number} y point
+ * @typedef  {Object} ShortestPath path 
+ * @property {GraphVertex[]} dist distances to vertex
+ * @property {GraphVertex[]} prev prev vertex
  */
 
 /**
- * A Star Path Finder
- * @param {number[][]} map matrix for map
- * @param {Point} start start point
- * @param {Point} end end point
- * @returns {Point[]} path
+ * ShortestPath
+ * @param {Graph} graph graph
+ * @param {GraphVertex} start start Vertex
+ * @param {GraphVertex} end end Vertex
+ * @param {(first:GraphVertex,second:GraphVertex)=>number} h heuristic function
+ * @returns {ShortestPath} path
  */
-function astar(map, start, end) {
-  let paths = []
-  return paths;
+function astar(graph, start, end, h) {
+  let dist = [];
+  let prev = [];
+  dist[start] = 0;
+  prev[start] = null;
+  let closed = new Set();
+  let open = new PriorityQueue(); // use as open
+  open.add(start, dist[start]);
+  while (!open.isEmpty && (!open.has(end))) {
+    /**
+     * @type {GraphVertex}
+     */
+    let u = open.pop();
+    if (!u) {
+      debugger;
+    }
+    for (const v of u.neighbors) {
+      if (!closed.has(v) && !open.has(v)) {
+        let edge = graph.findEdge(u, v) || graph.findEdge(v, u);
+        if (!edge) {
+          debugger;
+        }
+        if (!v) {
+          debugger;
+        }
+        // f = g + h
+        dist[v] = dist[u] + edge.weight;
+        prev[v] = u;
+        open.add(v, (dist[v] + h(v, end)));
+      }
+    }
+    closed.add(u);
+  }
+  return {
+    dist,
+    prev
+  };
 }
 
 module.exports = astar;
--- test/graph/graph.astar.test.js
+++ test/graph/graph.astar.test.js
@@ -7,10 +7,93 @@ const expect = require("chai").expect;
 
 const astar = require('../../src/graph/astar');
 
+/**
+ * matrixToGraph
+ * @param {number[][]]} matrix 
+ */
+function matrixToGraph(matrix) {
+  let graph = new Graph();
+  for (let i = 0; i < matrix.length; i++) {
+    const line = matrix[i];
+    for (let j = 0; j < line.length; j++) {
+      let vertexi_j = graph.findVertex(`${i}_${j}`) || new GraphVertex(`${i}_${j}`);
+      graph.addVertex(vertexi_j);
+      if (matrix[i][j] === 1) {
+        if (((j + 1) < line.length) && matrix[i][j + 1] === 1) {
+          let vertexi_j1 = graph.findVertex(`${i}_${j + 1}`) || new GraphVertex(`${i}_${j + 1}`);
+          let edge = new GraphEdge(vertexi_j, vertexi_j1, 1);
+          graph.addVertex(vertexi_j1).addEdge(edge);
+        }
+
+        if (((i + 1) < matrix.length) && matrix[i + 1][j] === 1) {
+          let vertexi1_j = graph.findVertex(`${i + 1}_${j}`) || new GraphVertex(`${i + 1}_${j}`);
+          let edge = new GraphEdge(vertexi_j, vertexi1_j, 1);
+          graph.addVertex(vertexi1_j).addEdge(edge);
+        }
+      }
+    }
+  }
+  return graph;
+}
+
+/**
+ * 
+ * @param {GraphVertex} first 
+ * @param {GraphVertex} second 
+ */
+function h(first, second) {
+  let [x1, y1] = first.value.split('_');
+  let [x2, y2] = second.value.split('_');
+  let xdist = Math.abs(x2 - x1);
+  let ydist = Math.abs(y2 - y1);
+  if (xdist > ydist) {
+    [ydist, xdist] = [xdist, ydist];
+  }
+  return xdist * 1.4 + (ydist - xdist);
+}
+
 describe('astar', () => {
-  it('should not find minimum paths from start to end', () => {})
+  it('should not find minimum paths from start to end', () => {
+    let graph = matrixToGraph([
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+      [1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 0, 1, 1]
+    ]);
+    let vertices = graph.allVertices;
+    let {
+      dist,
+      prev
+    } = astar(graph, vertices[0], vertices[vertices.length - 1], h);
+    expect(dist[vertices[vertices.length - 1]]).to.be.undefined;
+    expect(prev[vertices[vertices.length - 1]]).to.be.undefined;
 
-  it('should find minimum paths from start to end', () => {})
+  })
 
-  it('should find minimum paths from start to end #2', () => {})
+  it('should find minimum paths from start to end', () => {
+    let graph = matrixToGraph([
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    ]);
+    let vertices = graph.allVertices;
+    let {
+      dist,
+      prev
+    } = astar(graph, vertices[0], vertices[vertices.length - 1], h);
+    expect(dist[vertices[vertices.length - 1]]).to.be.equal(18);
+  });
 })
